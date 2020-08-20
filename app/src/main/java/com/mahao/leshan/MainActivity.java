050bfeb4533ff595e2c269684fe63a12cac95b80
mahao <15182289532@163.com>
2020-08-20 16:14:44 +0800

    进度1

--- app/src/main/java/com/mahao/leshan/MainActivity.java
+++ app/src/main/java/com/mahao/leshan/MainActivity.java
@@ -1,10 +1,11 @@
 package com.mahao.leshan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void launchFoodActivity(View view) {
